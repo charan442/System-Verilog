38a8785cf0bb9ed0f458ae9418dbd903c6c5783a
charan442 <85566214+charan442@users.noreply.github.com>
2024-08-07 14:22:31 +0530

    Create design.sv

---
+++ SV Adder Testbench/design.sv
@@ -0,0 +1,18 @@
+module full_adder(
+  sum,
+  carry,
+  a,
+  b,
+  c);
+ 
+  input a,b,c;
+  
+  output wire sum,carry;
+  
+  assign sum = a ^ b ^ c;
+ 
+  assign carry = (a & b) | (b & c) | (c & a);
+  
+endmodule
+  
+  
