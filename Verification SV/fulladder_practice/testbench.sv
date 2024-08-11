34d9330c5579aec97cd6cb88e49fafcc221eb683
charan442 <85566214+charan442@users.noreply.github.com>
2024-08-11 21:03:31 +0530

    Create testbench.sv

---
+++ Verification SV/fulladder_practice/testbench.sv
@@ -0,0 +1,21 @@
+`include "interface.sv"
+`include "test.sv"
+
+module testbench;
+  intf intff();
+  test tst (intff);
+  
+  full_adder FA(
+    .a(intff.a),
+    .b(intff.b),
+    .c(intff.c),
+    .sum(intff.sum),
+    .carry(intff.carry)
+  );
+  
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
