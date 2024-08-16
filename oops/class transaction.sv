9e179e02ae2db2a96b793c31ccdc31a452b150fa
charan442 <85566214+charan442@users.noreply.github.com>
2024-08-16 10:24:35 +0530

    Update class transaction.sv

--- oops/class transaction.sv
+++ oops/class transaction.sv
@@ -1,16 +1,17 @@
 class transaction;
-  bit[31:0] data;
+  bit[31:0]data;
 endclass
 
 module class_example;
   transaction tr1, tr2;
-  initial begin 
+  
+  initial begin
     tr1 = new();
-    tr1.data = 5;
-    tr2 = tr1;
+    tr1.data = 6;
+    tr2=tr1;
     $display("tr1.data = %0h",tr1.data);
     $display("tr2.data = %0h",tr2.data);
-    tr2.data = 10;
+    tr2.data = 15;
     $display("tr1.data = %0h",tr1.data);
     $display("tr2.data = %0h",tr2.data);
   end
