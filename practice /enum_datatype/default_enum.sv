c6943044725d0c3ddcb60468e7c5cc9fd5b8538e
charan442 <85566214+charan442@users.noreply.github.com>
2024-09-12 09:54:54 +0530

    Create default_enum.sv

---
+++ practice /enum_datatype/default_enum.sv
@@ -0,0 +1,17 @@
+module default_enum;
+  enum {INDIA,LANKA,TURKEY,JAPAN,CHINA}country;
+  
+  initial begin
+    $display("\n//county ={\n INDIA,\n LANKA,\n TURKEY,\n JAPAN,\n CHINA\n}");
+    
+    country=country.first;
+    
+    $display("");
+    
+    for(int i=0; i<5; i++) begin
+      $display("Country name = %0s and its default value is = %0d",country.first,country);
+      country=country.first;
+    end
+    $display("");
+  end
+endmodule
